d3028ddb30486b9f8198add17e54d9a8bdd0475c
Knz17 <nbllokt17@gmail.com>
2025-04-16 19:53:56 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
   
       const nama = form.querySelector("input[name='nama']").value;
       const barang = form.querySelector("input[name='barang']").value;
+      const lokasi1 = form.querySelector("input[name='lokasi1']").value;
+      const lokasi2 = form.querySelector("input[name='lokasi2']").value;
       const catatan = form.querySelector("textarea[name='catatan']").value;
   
       const nomorAdmin = "628888034201"; // Ganti dengan nomor admin kamu
-      const pesan = `Halo kak! Saya ${nama}, mau titip: ${barang}. Catatan: ${catatan}`;
+      const pesan = `Halo kak! Saya ${nama}, mau titip: ${barang}, penjemputan ${lokasi1}, pengiriman ${lokasi2}. Catatan: ${catatan}`;
       const urlWA = `https://wa.me/${nomorAdmin}?text=${encodeURIComponent(pesan)}`;
   
       window.open(urlWA, '_blank');
